0d2d5106c7bde502ac6df9009fb47300c1b62e75
johnsonyue <johnsonyuehit@gmail.com>
2016-11-03 13:42:18 +0800

    added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+1. chmod +x ss.sh
+2. run ss.sh
+3. go to system preferences -> network -> proxy
+   add proxy -> set method to auto -> set url to the output of ss.sh -> apply globally
--- ss.sh
+++ ss.sh
@@ -8,3 +8,4 @@ pip install --upgrade genpac
 kill `ps -ef | grep sslocal | awk '{print $1}'`
 sslocal -c `pwd`"/ss.cfg" -d start
 genpac -p "SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" --output="autoproxy.pac" --gfwlist-url="https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt"
+echo `pwd`"/autoproxy.pac"
