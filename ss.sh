95c3f393e558e00ab478872be5e7555e99278ea1
johnsonyue <johnsonyuehit@gmail.com>
2016-11-04 15:13:35 +0800

    using tsocks to pull pac

--- ss.sh
+++ ss.sh
@@ -5,7 +5,9 @@ apt-get install python-setuptools m2crypto
 pip install shadowsocks
 pip install genpac
 pip install --upgrade genpac
-kill `ps -ef | grep sslocal | awk '{print $1}'`
+kill `ps -ef | grep sslocal | awk '{print $2}'`
 sslocal -c `pwd`"/ss.cfg" -d start
-genpac -p "SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" --output="autoproxy.pac" --gfwlist-url="https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt"
+apt-get install tsocks
+sed -i 's/^server = .*/server = 127.0.0.1/' /etc/tsocks.conf
+tsocks genpac -p "SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" --output="autoproxy.pac" --gfwlist-url="https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt"
 echo `pwd`"/autoproxy.pac"
